66d0f740079381548f4cd6fdc11a19968413606f
doni255 <wijayadoni37@gmail.com>
2025-01-02 18:54:19 +0700

    updated

--- vite.config.js
+++ vite.config.js
@@ -18,17 +18,29 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss"; // Import Tailwind CSS
-import autoprefixer from "autoprefixer"; // Import Autoprefixer
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
       plugins: [
-        tailwindcss(), // Use imported tailwindcss
-        autoprefixer(), // Use imported autoprefixer
+        tailwindcss(),
+        autoprefixer(),
       ],
     },
   },
+  server: {
+    proxy: {
+      // Proxy all requests starting with '/api' to your backend
+      '/api': {
+        target: 'https://backendtokomesin.grhapengharapan.org', // Your backend URL
+        changeOrigin: true, // Modify the origin of the request to match the target
+        secure: true, // If the backend uses HTTPS, set to true
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove '/api' prefix before forwarding
+      },
+    },
+  },
 });
+
